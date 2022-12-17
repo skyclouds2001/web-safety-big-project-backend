bf1aa918c074c0fb6965edd38bb52e25b74961ae
skyclouds2001 <skyclouds2001@163.com>
2022-12-17 17:50:18 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# web-safety-big-project-backend
+ 
---
+++ src/main/java/org/fly/sky/WebSafetyBigProjectApplication.java
@@ -0,0 +1,13 @@
+package org.fly.sky;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebSafetyBigProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebSafetyBigProjectApplication.class, args);
+    }
+
+}
