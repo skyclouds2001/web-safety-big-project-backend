7cdd77a14e5df9e5058f03ef8b9b991eb6af7fe7
skyclouds2001 <skyclouds2001@163.com>
2022-12-18 22:40:10 +0800

    feat: add sql init file

---
+++ db/init.sql
@@ -0,0 +1,65 @@
+DROP DATABASE IF EXISTS dev;
+CREATE DATABASE dev;
+
+USE dev;
+
+DROP TABLE IF EXISTS scenery_spot;
+
+CREATE TABLE scenery_spot  (
+     `id` int UNSIGNED NOT NULL AUTO_INCREMENT COMMENT '景点ID',
+     `name` varchar(255) NOT NULL COMMENT '景点名称',
+     `desc` varchar(255) NULL COMMENT '景点描述',
+     `area` varchar(255) NOT NULL COMMENT '景点地点 格式-省市县',
+     `createTimestamp` bigint UNSIGNED NOT NULL COMMENT '景点创建时间戳',
+     `updateTimestamp` bigint UNSIGNED NOT NULL COMMENT '景点更新时间戳',
+     PRIMARY KEY (`id`)
+);
+
+DROP TABLE IF EXISTS ticket;
+
+CREATE TABLE ticket  (
+    `id` int UNSIGNED NOT NULL AUTO_INCREMENT COMMENT '门票ID',
+    `name` varchar(255) NOT NULL COMMENT '门票名称',
+    `desc` varchar(255) NULL COMMENT '门票描述',
+    `type` tinyint UNSIGNED NOT NULL COMMENT '门票类型 0-成人票 1-儿童票 2-其他' CHECK ( type IN (0, 1, 2) ),
+    `price` decimal(10, 2) UNSIGNED NOT NULL COMMENT '门票价格',
+    `scenerySpotId` int UNSIGNED NOT NULL COMMENT '所属景点ID',
+    `createTimestamp` bigint UNSIGNED NOT NULL COMMENT '门票创建时间戳',
+    `updateTimestamp` bigint UNSIGNED NOT NULL COMMENT '门票更新时间戳',
+    PRIMARY KEY (`id`)
+);
+
+DROP TABLE IF EXISTS `order`;
+
+CREATE TABLE `order`  (
+     `id` int UNSIGNED NOT NULL AUTO_INCREMENT COMMENT '订单ID',
+     `time` int UNSIGNED NOT NULL COMMENT '预定日期 格式YYYYMMSS' CHECK ( time >= 10000000 AND time <= 99999999 ),
+     `ticketId` int UNSIGNED NOT NULL COMMENT '所属门票ID',
+     `amount` int UNSIGNED NOT NULL COMMENT '购买门票数量',
+     `cost` decimal(10, 2) UNSIGNED NOT NULL COMMENT '订单总金额',
+     `customerId` int UNSIGNED NOT NULL COMMENT '所属客户ID',
+     `note` varchar(255) NULL COMMENT '订单备注',
+     `createTimestamp` bigint UNSIGNED NOT NULL COMMENT '订单创建时间戳',
+     `updateTimestamp` bigint UNSIGNED NOT NULL COMMENT '订单更新时间戳',
+     PRIMARY KEY (`id`)
+);
+
+DROP TABLE IF EXISTS user;
+
+CREATE TABLE user  (
+     `id` int UNSIGNED NOT NULL AUTO_INCREMENT COMMENT '用户ID',
+     `name` varchar(255) NOT NULL COMMENT '用户姓名',
+     `sex` tinyint NULL COMMENT '用户性别' CHECK ( sex in (0, 1) ),
+     `birth` varchar(255) NULL COMMENT '用户出生日期',
+     `phone` varchar(255) NOT NULL COMMENT '用户电话号码',
+     `desc` varchar(255) NULL COMMENT '门票描述',
+     `email` varchar(255) NULL COMMENT '用户邮箱',
+     `identity` varchar(255) NOT NULL COMMENT '用户身份证号码',
+     `address` varchar(255) NULL COMMENT '用户居住地址',
+     `type` tinyint UNSIGNED NOT NULL COMMENT '用户类型 0-普通用户 1-管理员' CHECK ( type IN (0, 1) ),
+     `nickname` varchar(255) NOT NULL COMMENT '账号昵称',
+     `password` varchar(255) NOT NULL COMMENT '账号密码',
+     `createTimestamp` bigint UNSIGNED NOT NULL COMMENT '客户创建时间戳',
+     `updateTimestamp` bigint UNSIGNED NOT NULL COMMENT '客户更新时间戳',
+     PRIMARY KEY (`id`)
+);
---
+++ db/test.sql
@@ -0,0 +1,17 @@
+USE DEV;
+
+DELETE FROM scenery_spot WHERE TRUE;
+
+INSERT INTO scenery_spot (name, `desc`, area, createTimestamp, updateTimestamp) VALUES ('黄山', 'huang-shan', '安徽省黄山市', now(), now());
+
+DELETE FROM ticket WHERE TRUE;
+
+INSERT INTO ticket (name, `desc`, type, price, scenerySpotId, createTimestamp, updateTimestamp) VALUES ('成人普票', 'person', 0, 100, 1, now(), now());
+
+DELETE FROM `order` WHERE TRUE;
+
+INSERT INTO `order` (time, ticketId, amount, cost, customerId, note, createTimestamp, updateTimestamp) VALUES ('20221201', 1, 1, 100, 1, 'order', now(), now());
+
+DELETE FROM user WHERE TRUE;
+
+INSERT INTO user (name, sex, birth, phone, email, identity, address, type, nickname, password, `desc`, createTimestamp, updateTimestamp) VALUES ('张三', 0, '20200101', '13888888888', 'zhangsan@666.com', '331082202001010019', '河南省郑州市中原区', 1, 'zhang', 'zhang', 'zhang-san', now(), now());
