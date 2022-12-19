7f356fa7895db499d36bbf2d559e91d0fe5cfb1f
skyclouds2001 <skyclouds2001@163.com>
2022-12-19 16:16:41 +0800

    feat: add ScenerySpot services

--- src/main/java/org/fly/sky/service/ScenerySpotService.java
+++ src/main/java/org/fly/sky/service/ScenerySpotService.java
@@ -1,4 +1,19 @@
 package org.fly.sky.service;
 
+import org.fly.sky.domain.ScenerySpot;
+
+import java.util.List;
+
 public interface ScenerySpotService {
+
+    ScenerySpot getById(Integer id);
+
+    List<ScenerySpot> getAll();
+
+    boolean save(ScenerySpot scenerySpot);
+
+    boolean update(ScenerySpot scenerySpot);
+
+    boolean delete(Integer id);
+
 }
--- src/main/java/org/fly/sky/service/impl/ScenerySpotServiceImpl.java
+++ src/main/java/org/fly/sky/service/impl/ScenerySpotServiceImpl.java
@@ -1,9 +1,12 @@
 package org.fly.sky.service.impl;
 
 import org.fly.sky.dao.ScenerySpotDao;
+import org.fly.sky.domain.ScenerySpot;
 import org.fly.sky.service.ScenerySpotService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class ScenerySpotServiceImpl implements ScenerySpotService {
@@ -11,4 +14,28 @@ public class ScenerySpotServiceImpl implements ScenerySpotService {
     @Autowired
     private ScenerySpotDao scenerySpotDao;
 
+    @Override
+    public ScenerySpot getById(Integer id) {
+        return scenerySpotDao.getById(id);
+    }
+
+    @Override
+    public List<ScenerySpot> getAll() {
+        return scenerySpotDao.getAll();
+    }
+
+    @Override
+    public boolean save(ScenerySpot scenerySpot) {
+        return scenerySpotDao.save(scenerySpot) > 0;
+    }
+
+    @Override
+    public boolean update(ScenerySpot scenerySpot) {
+        return scenerySpotDao.update(scenerySpot) > 0;
+    }
+
+    @Override
+    public boolean delete(Integer id) {
+        return scenerySpotDao.delete(id) > 0;
+    }
 }
