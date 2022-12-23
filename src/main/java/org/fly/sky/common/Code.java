547129fd7a12dec74f4cdb6502c73c87dc78280a
skyclouds2001 <skyclouds2001@163.com>
2022-12-23 12:20:49 +0800

    feat: add code enum

--- src/main/java/org/fly/sky/common/Code.java
+++ src/main/java/org/fly/sky/common/Code.java
@@ -13,8 +13,8 @@ public enum Code {
     INCORRECT_TYPE_PARAM(10000, "参数类型错误"),
     INCORRECT_RANGE_PARAM(10001, "参数范围错误"),
     MISSING_NECESSARY_PARAM(10002, "缺少的必需参数"),
-    EMPTY_STRING_PARAM(10003, "字符串类参数不能为空"),
-    INCORRECT_INDEX_PARAM(10001, "索引参数必须为正整数"),
+    EMPTY_STRING_PARAM(10003, "字符串参数不能为空"),
+    INCORRECT_INDEX_PARAM(10004, "索引参数必须为正整数"),
     FAIL_INSERT_SQL_OPERATE(20000, "数据库写入失败"),
     FAIL_SELECT_SQL_OPERATE(20001, "数据库读取失败"),
     FAIL_UPDATE_SQL_OPERATE(20002, "数据库更新失败"),
@@ -25,7 +25,10 @@ public enum Code {
     INCORRECT_PASSWORD_FAILURE(30002, "密码错误"),
     INVALID_LENGTH_PASSWORD_FAILURE(30003, "密码长度需超过8"),
     INVALID_CHARACTER_PASSWORD_FAILURE(30004, "密码仅能由数字和大小写字母构成"),
-    MISSING_CHARACTER_PASSWORD_FAILURE(30005, "密码中必须包含数字和字母");
+    MISSING_CHARACTER_PASSWORD_FAILURE(30005, "密码中必须包含数字和大小写字母中的三种"),
+    INVALID_EMAIL_FAILURE(30006, "邮箱格式非法"),
+    INVALID_PHONE_FAILURE(30007, "电话格式非法"),
+    INVALID_IDENTITY_FAILURE(30008, "身份证号码格式非法");
 
     private boolean success;
 
