4f42d73469affc6e26ca045b651328f947523ab2
skyclouds2001 <skyclouds2001@163.com>
2022-12-23 12:22:42 +0800

    feat: add phone&email&password check

---
+++ src/main/java/org/fly/sky/util/CheckValidate.java
@@ -0,0 +1,21 @@
+package org.fly.sky.util;
+
+import java.util.regex.Pattern;
+
+public class CheckValidate {
+
+    private CheckValidate() {}
+
+    public static boolean isValidatePhone(String phone) {
+        return Pattern.matches("^1[3-9]\\d{9}$", phone);
+    }
+
+    public static boolean isValidateEmail(String email) {
+        return Pattern.matches("[a-zA-Z0-9_]+@[a-zA-Z0-9_]+(\\.[a-zA-Z0-9]+)+", email);
+    }
+
+    public static boolean isValidatePassword(String password) {
+        return Pattern.matches("^(?![0-9]+$)(?![a-zA-Z]+$)[0-9A-Za-z]{6,20}$", password);
+    }
+
+}
