20c9294017c628cb0b8c4ae6d7a0d178abdff9c7
skyclouds2001 <skyclouds2001@163.com>
2022-12-28 15:50:12 +0800

    feat: update interceptor config

--- src/main/java/org/fly/sky/interceptor/GlobalInterceptor.java
+++ src/main/java/org/fly/sky/interceptor/GlobalInterceptor.java
@@ -2,6 +2,9 @@ package org.fly.sky.interceptor;
 
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.fly.sky.common.Code;
+import org.fly.sky.exception.CustomException;
+import org.fly.sky.util.GenerateToken;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
@@ -11,6 +14,10 @@ public class GlobalInterceptor implements HandlerInterceptor {
 
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        System.out.println(request.getHeader("Authorization"));
+        String authorization = request.getHeader("Authorization");
+        if (!GenerateToken.hasToken(authorization))
+            throw new CustomException(Code.NEED_LOGIN_FAILURE);
         return HandlerInterceptor.super.preHandle(request, response, handler);
     }
 
